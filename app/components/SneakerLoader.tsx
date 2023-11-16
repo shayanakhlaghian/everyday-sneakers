0a207e4b39eea79fceb9a177a3eacc9165992208
Shayan <shayanakhlaghian@gmail.com>
2023-11-16 17:00:40 +0330

    fix progress value

--- app/components/SneakerLoader.tsx
+++ app/components/SneakerLoader.tsx
@@ -3,15 +3,17 @@ import { Html, useProgress } from '@react-three/drei';
 
 const CanvasLoader = () => {
   const { progress } = useProgress();
+
+  const rounded = Math.round(progress);
   return (
     <Html as='div' center>
       <div
         className='radial-progress bg-primary text-primary-content border-4 border-primary lg:left-16 top-12 lg:top-0'
         // @ts-ignore
-        style={{ '--value': Math.round(progress) }}
+        style={{ '--value': rounded }}
         role='progressbar'
       >
-        70%
+        {rounded}%
       </div>
     </Html>
   );
